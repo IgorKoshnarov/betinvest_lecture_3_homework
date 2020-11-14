2c94762d6366dca5ae834cc40d8f076930dab188
Igor <igor.koshnarov@gmail.com>
2020-11-14 16:39:02 +0200

    Fix wrong argument type in bs03:split function

--- bs03.erl
+++ bs03.erl
@@ -2,7 +2,7 @@
 -export([split/2]).
 
 split(Bin, Splitter) ->
-    reverse_and_clean(split(Bin, Splitter, <<>>, <<>>, [])).
+    reverse_and_clean(split(Bin, list_to_bin(Splitter), <<>>, <<>>, [])).
 
 split(Bin, <<>>, Splitter, Word, List) ->
     split(Bin, Splitter, <<>>, <<>>, [Word | List]);
@@ -12,6 +12,15 @@ split(<<A/utf8, Rest/binary>>, Rest1, Bin, Word, List) ->
     split(Rest, <<Bin/binary, Rest1/binary>>, <<>>, <<Word/binary, Bin/binary, A/utf8>>, List);
 split(<<>>, _, Bin, Word, List) ->
     [<<Word/binary, Bin/binary>> | List].
+
+list_to_bin(L) ->
+    list_to_bin(L, <<>>).
+
+list_to_bin([H | T], Acc) ->
+    list_to_bin(T, <<H/utf8, Acc/binary>>);
+list_to_bin([], Acc) ->
+    Acc.
+
 
 reverse_and_clean(L) ->
     reverse_and_clean(L, []).
